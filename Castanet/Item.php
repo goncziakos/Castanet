822ae0d6d778652bd64e95d2b91be6d0debf8f42
Ákos Gönczi <32277563+goncziakos@users.noreply.github.com>
2024-03-21 11:03:07 +0100

    Update Item.php

--- Castanet/Item.php
+++ Castanet/Item.php
@@ -49,7 +49,7 @@
  * @license   http://www.opensource.org/licenses/mit-license.html MIT License
  * @link      https://github.com/silverorange/castanet
  */
-class Castanet_Item
+class Item
 {
     // {{{ protected properties
 
